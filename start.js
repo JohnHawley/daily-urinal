1e3d1034bd144bb890dd2bfb55eb791c913f95d4
John Hawley <john@hawley.email>
2016-03-11 13:49:47 -0600

    Google Added, Large Update to 0.3

--- start.js
+++ start.js
@@ -3,6 +3,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 //Added as a workaround for bad SSL Certs
 
 // ---- Requires  ---- //
+var phantom = require('x-ray-phantom');
 var Xray = require('x-ray');
 var fs = require('fs');
 var exec = require('child_process').exec;
@@ -12,7 +13,7 @@ var factOn = true;
 
 
 // ---- Globals  ---- //
-var x = Xray();
+var x = Xray(); //.driver(phantom());
 var printFile = 'print.html';
 var d = new Date();
 var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -27,14 +28,18 @@ function scrapeInOrder(cb) {
   console.log("Scraping data...");
   getHistory(function(dataHistory) {
     getWord(function(dataWord) {
-      getetymology((function(dataetymology) {
-        getWeather(function(dataWeather) {
-          getFact(function(dataFact) {
-            getNews(function(dataNews) {
-              buildHtml(dataHistory, dataWord, dataetymology, dataWeather, dataFact, dataNews, cb);
+      getEtymology((function(dataEtymology) {
+        getGoogleWord((function(dataGoogleWord) {
+          getWeather(function(dataWeather) {
+            getFact(function(dataFact) {
+              getNews(function(dataNews) {
+                getGoogleQA(function(dataQA) {
+                  buildHtml(dataHistory, dataWord, dataEtymology, dataGoogleWord, dataWeather, dataFact, dataNews, dataQA, cb);
+                });
+              });
             });
           });
-        });
+        }), dataWord);
       }), dataWord);
     });
   });
@@ -51,6 +56,7 @@ function getHistory(cb) {
     excrept1: 'article.article p:nth-child(1)',
     excrept2: 'article.article p:nth-child(2)'
   }])(function(err, obj) {
+    obj[0].excrept1 = obj[0].excrept1.substring(0, obj[0].excrept1.indexOf('.'));
     fs.writeFile(data, JSON.stringify(obj, null, 2), "utf-8", function(err) {
       fs.readFile(data, function(err, result) {
         cb(JSON.parse(result));
@@ -82,13 +88,24 @@ function getWord(cb) {
 
 // ---- Get Google Word Data ---- // // --- Careful with this scrape.. it's Google
 function getGoogleWord(cb, word) {
-  var url = "https://www.google.com/search?q=define%20" + word;
+  var url = "https://www.google.com/search?q=define+" + word[0].word;
   var data = "data/googleWord.json";
-  x(url, 'div.lr_container.mod', [{
-    origin: '#lr_dct_img_origin_' + word + '0@src',
-    type: '#lr_dct_img_use@src'
+  x(url, '#lfoot', [{
+    origin: 'script@html'
   }])(function(err, obj) {
-    fs.writeFile(data, JSON.stringify(obj, null, 2), "utf-8", function(err) {
+    if (obj == '') {
+      obj = [{
+        origin: 'Nothing found'
+      }];
+    }
+    var regex = /\_image\_src\=\'(.*?)\'\;/g,
+      item, matches = [];
+    while (item = regex.exec(obj[0].origin)) {
+      matches.push(item[1]);
+    }
+    obj[0].origin = matches[0].replace('\\075', '=').replace('\\75', '=');
+    obj[0].history = matches[1].replace('\\075', '=').replace('\\75', '=');
+    fs.writeFile(data, JSON.stringify(obj, null, 2), "ascii", function(err) {
       fs.readFile(data, function(err, result) {
         cb(JSON.parse(result));
       });
@@ -98,14 +115,17 @@ function getGoogleWord(cb, word) {
 
 
 // ---- Get Word etymology Data ---- //
-function getetymology(cb, word) {
+function getEtymology(cb, word) {
   var url = "http://www.etymonline.com/index.php?allowed_in_frame=0&search=" + word[0].word.replace(" ", "+");
   var data = "data/etymology.json";
   x(url, '#dictionary', [{
     etymology: 'dd.highlight'
   }])(function(err, obj) {
-    if (obj=='') {obj = [{etymology: 'No etymology found'}];}
-    console.log(err + "-----" + obj);
+    if (obj == '') {
+      obj = [{
+        etymology: 'No etymology found'
+      }];
+    }
     fs.writeFile(data, JSON.stringify(obj, null, 2), "utf-8", function(err) {
       fs.readFile(data, function(err, result) {
         cb(JSON.parse(result));
@@ -166,10 +186,32 @@ function getNews(cb) {
   });
 }
 
+// ---- Get Google QA Data ---- // // --- Careful with this scrape.. it's Google
+function getGoogleQA(cb) {
+  var url = "https://www.google.com/search?q=i%27m+feeling+curious";
+  var data = "data/googleQA.json";
+  x(url, '._Wtj', [{
+    q: '._cNh',
+    a: '._dNh'
+  }])(function(err, obj) {
+    if (obj == '') {
+      obj = [{
+        q: 'Nothing found',
+        a: 'Nothing found'
+      }];
+    }
+    fs.writeFile(data, JSON.stringify(obj, null, 2), "utf-8", function(err) {
+      fs.readFile(data, function(err, result) {
+        cb(JSON.parse(result));
+      });
+    });
+  });
+}
+
 
 
 // ---- Build HTML File ---- //
-function buildHtml(history, word, etymology, weather, fact, news, cb) {
+function buildHtml(history, word, etymology, googleWord, weather, fact, news, qa, cb) {
 
   var head = `
   <head>
@@ -177,7 +219,7 @@ function buildHtml(history, word, etymology, weather, fact, news, cb) {
     <link href="assets/style.css" rel="stylesheet" />
   </head>
   `,
-  header = `
+    header = `
   <div class="row">
     <div class="col-xs-12">
       <img src="assets/urinal-logo.png" />
@@ -199,7 +241,7 @@ function buildHtml(history, word, etymology, weather, fact, news, cb) {
   /* ==============================
      =        BUILD HISTORY       =
      ============================== */
-     var htmlHistory = `
+  var htmlHistory = `
      <h3>Today in History</h3>
      <div class="well">
         <div class="year">${history[0].year}</div>
@@ -212,10 +254,10 @@ function buildHtml(history, word, etymology, weather, fact, news, cb) {
      `;
 
 
- /* ==============================
-    =        BUILD WEATHER       =
-    ============================== */
-     var htmlWeather = `
+  /* ==============================
+     =        BUILD WEATHER       =
+     ============================== */
+  var htmlWeather = `
      <h3>WEATHER</h3>
      <div class="well">
        <div class="row">
@@ -234,37 +276,47 @@ function buildHtml(history, word, etymology, weather, fact, news, cb) {
      `;
 
 
- /* ==============================
-    =        BUILD Word          =
-    ============================== */
-     var htmlWord = `
+  /* ==============================
+     =        BUILD Word          =
+     ============================== */
+  var htmlEtymology;
+  if (word[0].def2) {
+    htmlEtymology = `
+           <p>${word[0].def1}</p>
+           <p>${word[0].def2}</p>
+           <p class="etom">${etymology[0].etymology}</p>
+           `;
+  } else {
+    htmlEtymology = `
+          <p>
+          <strong>1</strong>${word[0].def1}</p>
+          <p class="etom">${etymology[0].etymology}</p>
+          `;
+  }
+
+  var htmlWord = `
      <h3>Word of the day</h3>
      <div class="well">
        <h2 style="color:#555;margin: 0;">${word[0].word}</h2>
        <span>(<em>${word[0].type}</em>)</span>
-       ${word[0].pro}
+        ${word[0].pro}
        <hr style="margin:10px 0;">
-     `;
-     if (word[0].def2) {
-       htmlWord += `
-        <p>${word[0].def1}</p>
-        <p>${word[0].def2}</p>
-        <p class="etom">${etymology[0].etymology}</p>
-       </div>`;
-     } else {
-       htmlWord += `
-       <p>
-       <strong>1</strong>${word[0].def1}</p>
-       <p class="etom">${etymology[0].etymology}</p>
+        ${htmlEtymology}
+        <p style="margin:0;">
+          <img src="${googleWord[0].origin}" style="width:100%;"/>
+        </p>
+        <p>
+          <img src="${googleWord[0].history}" style="width:100%;"/>
+        </p>
        </div>
-       `;
-     }
+     `;
 
- /* ==============================
-    =        BUILD Fact         =
-    ============================== */
-    if (factOn) {
-      var htmlFact = `
+
+  /* ==============================
+     =        BUILD Fact         =
+     ============================== */
+  if (factOn) {
+    var htmlFact = `
       <div class="col-xs-12">
         <h3>RANDOM FACT</h3>
         <div class="well fact">
@@ -272,33 +324,49 @@ function buildHtml(history, word, etymology, weather, fact, news, cb) {
         </div>
       </div>
       `;
-    } else {
-      var htmlFact = '';
-    }
+  } else {
+    var htmlFact = '';
+  }
 
 
- /* ==============================
-    =        BUILD News          =
-    ============================== */
-     var htmlNews = `
+  /* ==============================
+     =        BUILD News          =
+     ============================== */
+  var htmlNews = `
      <h3>Headlines</h3>
      <div class="well news">
      `;
-     for (var i = 0; ((i < news.length) && (i < 6)); i++) {
-       var story = news[i];
-       htmlNews += `
+  for (var i = 0;
+    ((i < news.length) && (i < 3)); i++) {
+    var story = news[i];
+    htmlNews += `
          <div>${story.headline}</div>
          <p>${story.src}</p>
        `
-     }
-     htmlNews += "</div>";
+  }
+  htmlNews += "</div>";
+
+
+  /* ==============================
+     =        BUILD Q&A         =
+     ============================== */
+
+    var htmlFact = `
+      <div class="col-xs-12">
+        <h3>TODAY'S Q&A</h3>
+        <div class="well QA">
+         <p>${qa[0].q}</p>
+         <p>${qa[0].a}</p>
+        </div>
+      </div>
+      `;
 
 
 
-   /* ==============================
-      =        B O D Y             =
-      ============================== */
-      var body = `
+  /* ==============================
+     =        B O D Y             =
+     ============================== */
+  var body = `
       ${header}
       <div class="row">
         <div class="col-xs-6">
@@ -306,25 +374,26 @@ function buildHtml(history, word, etymology, weather, fact, news, cb) {
             ${htmlNews}
           </div>
           <div class="col-xs-12">
-            ${htmlWeather}
+            ${htmlHistory}
           </div>
+          ${htmlFact}
         </div>
         <div class="col-xs-6">
           <div class="col-xs-12">
-            ${htmlHistory}
+            ${htmlWeather}
           </div>
           <div class="col-xs-12">
             ${htmlWord}
           </div>
-            ${htmlFact}
+
         </div>
       </div>
       `
 
-   /* ==============================
-      =        HTML                =
-      ============================== */
-      var html = `
+  /* ==============================
+     =        HTML                =
+     ============================== */
+  var html = `
       <html>
       ${head}
         <body>
